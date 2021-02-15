373bd69f0b01c6ef8380931fcad1d2a0ff17532c
ludmiladev <ljuda12@gmail.com>
2021-02-15 13:31:51 +0200

    добавила в проэкт npm install express morgan cors

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-const contacts = require('./contacts.js');
-const argv = require('yargs').argv;
 const express = require('express');
 const cors = require('cors');  
 const morgan = require('morgan');
+require('dotenv').config();
 
+const contactRouter = require('./api/contacts/contactRouters');
 
-function invokeAction({ action, id, name, email, phone }) {
-    switch (action) {
-        case "list":
-            console.table(contacts.listContacts());
-            break;
+const ContactServer = require('./api/server');
 
-        case "get":
-            console.log(contacts.getContactById(id));
-            break;
-
-        case "add":
-            contacts.addContact(name, email, phone);
-            console.log(contacts.listContacts);
-            break;
-
-        case "remove":
-            contacts.removeContact(id);
-            console.log(contacts.listContacts);
-            break;
-
-        default:
-            console.warn("\x1B[31m Unknown action type!");
-    }
-}
-
-invokeAction(argv);
+new ContactServer().start();
