dd6b2ecac20767b1aa2ca129c40690b3c24c20d3
ludmiladev <ljuda12@gmail.com>
2021-03-03 01:45:52 +0200

    done

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,3 +1,4 @@
+
 const { Router } = require('express');
 const UserController = require('../controllers/users.controllers');
 const { authorize } = require('../controllers/authController.js');
@@ -8,10 +9,26 @@ router.post(
     '/auth/register',
     UserController.validateUser,
     UserController.registerNewUser,
+    UserController.mailingСheck,
 );
 router.post('/auth/login', UserController.validateUser, UserController.login);
 router.get('/auth', authorize, UserController.authUser);
 router.get('/auth/logout/:userId', authorize, UserController.logout);
 router.get('/users/current', authorize, UserController.getCurrentUser);
 
-module.exports = router;
+// router.post('/users',
+//   UserController.upload.single('userAvatar'),
+//   (req, res) => {res.send({ file: req.file, ...req.body });},);
+
+router.patch(
+    '/users/userAvatar',
+    authorize,
+    UserController.upload.single('userAvatar'),
+    UserController.validateUpdateUser,
+    UserController.updateUserAvatar,
+),
+    (module.exports = router);
+router.get(
+    '/auth/verify/:verificationToken',
+    UserController.userVerificationToken,
+);
